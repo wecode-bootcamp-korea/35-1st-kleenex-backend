d857df023a1c5a1140f6d49e235d68a49f8406af
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-22 14:11:19 +0900

    Create: products view(product list)

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from products.views import MainProductView
+from products.views import MainProductView, CoffeeProductView
 
 urlpatterns = [
     path('/main', MainProductView.as_view()),
+    path('', CoffeeProductView.as_view()),
+    path('/<int:coffee_category_id>', CoffeeProductView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http           import JsonResponse
 from django.views          import View
+from django.core.paginator import Paginator
 
 from products.models  import Product, ProductImage, TasteByProduct
 
@@ -42,3 +43,39 @@ class MainProductView(View):
             )
         
         return JsonResponse({'premium' : result_premium,'fresh_product' : result_fresh_product}, status = 200)
+
+
+class CoffeeProductView(View):
+    def get(self, request, coffee_category_id=None):
+        result_products = []
+        
+        if coffee_category_id == None:
+            products      = Product.objects.all()
+            paginator     = Paginator(products, 12)
+            page_number   = request.GET.get('page')
+            page_products = paginator.get_page(page_number)
+        
+        else:
+            products      = Product.objects.filter(subcategory_id = coffee_category_id)
+            paginator     = Paginator(products, 12)
+            page_number   = request.GET.get('page')
+            page_products = paginator.get_page(page_number)
+        
+        for product in page_products.object_list:
+            images = ProductImage.objects.filter(product_id=product.id)
+            flavors = TasteByProduct.objects.filter(product_id = product.id)
+            result_products.append(
+                {
+                    'name'         : product.name,
+                    'eng_name'     : product.eng_name,
+                    'img'          : [image.url for image in images],
+                    'taste'        : [flavor.taste.name for flavor in flavors],
+                    'roasting_date': product.roasting_date,
+                    'price'        : product.price
+                }
+            )
+            
+        return JsonResponse(
+            {'shop_product_list'   : result_products},
+            status = 200
+        )
