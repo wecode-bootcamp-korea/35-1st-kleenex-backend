7aba49181d826f86320797610770e8ec6dba4a67
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-22 10:26:54 +0900

    Create: products view(products list, product detail)

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from products.views import CoffeeProductView, MainProductView, ProductDetailView
+
+urlpatterns = [
+    path('/main', MainProductView.as_view()),
+    path('', CoffeeProductView.as_view()),
+    path('/<int:coffee_category_id>', CoffeeProductView.as_view()),
+    path('/detail/<int:product_id>', ProductDetailView.as_view())
+]
