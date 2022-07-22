81ced24554327080f80d0644fa06a51a3666f5f3
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-22 15:10:02 +0900

    Create: products view(product detail2)

--- products/views.py
+++ products/views.py
@@ -34,12 +34,12 @@ class MainProductView(View):
             flavors = TasteByProduct.objects.filter(product_id = fresh_product.id)
             result_fresh_product.append(
                 {
-                    'name'          : premium.name,
-                    'eng_name'      : premium.eng_name,
+                    'name'          : fresh_product.name,
+                    'eng_name'      : fresh_product.eng_name,
                     'img'           : [image.url for image in images],
-                    'roasting_date' : premium.roasting_date,
+                    'roasting_date' : fresh_product.roasting_date,
                     'taste'         : [flavor.taste.name for flavor in flavors],
-                    'price'         : premium.price
+                    'price'         : fresh_product.price
                 }
             )
         
