7c45553786049e893df713f19e1df00bfbc65d4c
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-22 16:38:56 +0900

    Create: products view(edit paginator)

--- products/views.py
+++ products/views.py
@@ -9,39 +9,28 @@ from products.models  import Product, ProductImage, TasteByProduct
 
 class MainProductView(View): 
     def get(self, request): 
-        premiums             = Product.objects.all().order_by('-price')
-        fresh_products       = Product.objects.all().order_by('-roasting_date')
-        result_premium       = []
-        result_fresh_product = []
-        
-        for premium in premiums[:3]: 
-            images = ProductImage.objects.filter(product_id = premium.id)
-            flavors  = TasteByProduct.objects.filter(product_id = premium.id)
-            result_premium.append(
-                {
+        premiums             = Product.objects.all().order_by('-price')[:3]
+        fresh_products       = Product.objects.all().order_by('-roasting_date')[:4]
+        result_premium       = [{
+                    'id'            : premium.id,
                     'name'          : premium.name,
                     'eng_name'      : premium.eng_name,
-                    'img'           : [image.url for image in images],
+                    'img'           : [image.url for image in ProductImage.objects.filter(product_id = premium.id)],
                     'roasting_date' : premium.roasting_date,
-                    'taste'         : [flavor.taste.name for flavor in flavors],
+                    'taste'         : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = premium.id)],
                     'price'         : premium.price
-                }
-            )
+                } for premium in premiums]
         
-        for fresh_product in fresh_products[:4]:
-            images = ProductImage.objects.filter(product_id = fresh_product.id)
-            flavors = TasteByProduct.objects.filter(product_id = fresh_product.id)
-            result_fresh_product.append(
-                {
+        result_fresh_product = [{
+                    'id'            : fresh_product.id,
                     'name'          : fresh_product.name,
                     'eng_name'      : fresh_product.eng_name,
-                    'img'           : [image.url for image in images],
+                    'img'           : [image.url for image in ProductImage.objects.filter(product_id = fresh_product.id)],
                     'roasting_date' : fresh_product.roasting_date,
-                    'taste'         : [flavor.taste.name for flavor in flavors],
+                    'taste'         : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = fresh_product.id)],
                     'price'         : fresh_product.price
-                }
-            )
-        
+                } for fresh_product in fresh_products]
+
         return JsonResponse({'premium' : result_premium,'fresh_product' : result_fresh_product}, status = 200)
 
 
