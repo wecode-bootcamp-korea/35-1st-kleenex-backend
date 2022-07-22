a1b5adf6bcf5f56bf39a2bafd84628e003a01d10
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-22 14:15:48 +0900

    Create: products view(product detail)

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from products.views import MainProductView, CoffeeProductView
+from products.views import MainProductView, CoffeeProductView, ProductDetailView
 
 urlpatterns = [
     path('/main', MainProductView.as_view()),
     path('', CoffeeProductView.as_view()),
     path('/<int:coffee_category_id>', CoffeeProductView.as_view()),
+    path('/detail/<int:product_id>', ProductDetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.http           import JsonResponse
 from django.views          import View
 from django.core.paginator import Paginator
 
-from products.models  import Product, ProductImage, TasteByProduct
+from products.models  import Product, ProductImage, TasteByProduct, Size, Grainding
 
 
 class MainProductView(View): 
@@ -79,3 +79,23 @@ class CoffeeProductView(View):
             {'shop_product_list'   : result_products},
             status = 200
         )
+
+
+class ProductDetailView(View): 
+    def get(self, request, product_id): 
+        product        = Product.objects.get(id=product_id)
+        flavors         = TasteByProduct.objects.filter(product_id = product.id)
+        grainding      = Grainding.objects.all()
+        sizes          = Size.objects.all()
+        product_images = ProductImage.objects.filter(product_id = product.id)
+        product_detail = (
+            {
+                'img'        : [image.url for image in product_images],
+                'name'       : product.name,
+                'price'      : product.price,
+                'tastes'     : [flavor.taste.name for flavor in flavors],
+                'graind_type': [graind.type for graind in grainding],
+                'size_type'  : [size.name for size in sizes]
+            }
+        )
+        return JsonResponse({'product_detial' : product_detail}, status = 200)
