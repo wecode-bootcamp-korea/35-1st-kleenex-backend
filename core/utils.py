2298dbc15134f8338af372bede7546c2e5f52810
apple <apple@apples-MacBook-Pro.local>
2022-07-22 16:28:43 +0900

    users app/urls.py and views.py (signin & decorator)

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,8 @@
+import jwt
 import re
+
+from django.conf import settings
+from django.http import JsonResponse
 
 from users.models import User
 
@@ -33,3 +37,21 @@ def duplicate_check_email(email):
 def duplicate_check_phone_number(phone_number):
     if User.objects.filter(phone_number = phone_number).exists():
         raise ValueError("INVAILD_PHONE_NUMBER")
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization')
+            payload      = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
+            user         = User.objects.get(id = payload['id'])
+            request.user = user
+
+            return func(self, request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status= 400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=400)
+
+    return wrapper
