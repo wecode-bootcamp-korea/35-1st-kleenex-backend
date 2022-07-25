38ff0ca46ef450c0d200237c40d8d8f975333629
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-25 17:10:49 +0900

    product list modify img_id, taste_id

--- products/views.py
+++ products/views.py
@@ -12,27 +12,35 @@ class MainProductView(View):
         fresh_products       = Product.objects.all().order_by('-roasting_date')[:4]
 
         result_premium       = [{
-                    'id'            : premium.id,
-                    'name'          : premium.name,
-                    'eng_name'      : premium.eng_name,
-                    'img'           : [image.url for image in ProductImage.objects.filter(product_id = premium.id)],
-                    'img_id'        : [image.id for image in ProductImage.objects.filter(product_id = premium.id)],
-                    'roasting_date' : premium.roasting_date,
-                    'taste'         : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = premium.id)],
-                    'taste_id'      : [flavor.taste.id for flavor in TasteByProduct.objects.filter(product_id = premium.id)],
-                    'price'         : premium.price
+                    'id'             : premium.id,
+                    'name'           : premium.name,
+                    'eng_name'       : premium.eng_name,
+                    'img'            : [{
+                        'img_id'     : image.id,
+                        'img_url'    : image.url
+                    } for image in ProductImage.objects.filter(product_id = premium.id)],
+                    'roasting_date'  : premium.roasting_date,
+                    'taste'          : [{
+                        'taste_id'   : flavor.taste.name,
+                        'taste_name' : flavor.taste.id
+                    } for flavor in TasteByProduct.objects.filter(product_id = premium.id)],
+                    'price'          : premium.price
                 } for premium in premiums]
         
         result_fresh_product = [{
-                    'id'            : fresh_product.id,
-                    'name'          : fresh_product.name,
-                    'eng_name'      : fresh_product.eng_name,
-                    'img'           : [image.url for image in ProductImage.objects.filter(product_id = fresh_product.id)],
-                    'img_id'        : [image.id for image in ProductImage.objects.filter(product_id = fresh_product.id)],
-                    'roasting_date' : fresh_product.roasting_date,
-                    'taste'         : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = fresh_product.id)],
-                    'taste_id'      : [flavor.taste.id for flavor in TasteByProduct.objects.filter(product_id = fresh_product.id)],
-                    'price'         : fresh_product.price
+                    'id'             : fresh_product.id,
+                    'name'           : fresh_product.name,
+                    'eng_name'       : fresh_product.eng_name,
+                    'img'            : [{
+                        'img_id'     : image.id,
+                        'img_url'    : image.url
+                    } for image in ProductImage.objects.filter(product_id = fresh_product.id)],
+                    'roasting_date'  : fresh_product.roasting_date,
+                    'taste'          : [{
+                        'taste_id'   : flavor.taste.name,
+                        'taste_name' : flavor.taste.id
+                    } for flavor in TasteByProduct.objects.filter(product_id = fresh_product.id)],
+                    'price'          : fresh_product.price
                 } for fresh_product in fresh_products]
 
         return JsonResponse({'premium' : result_premium,'fresh_product' : result_fresh_product}, status = 200)
@@ -76,10 +84,14 @@ class CoffeeProductView(View):
                     'id'           : product.id,
                     'name'         : product.name,
                     'eng_name'     : product.eng_name,
-                    'img'          : [image.url for image in ProductImage.objects.filter(product_id=product.id)],
-                    'img_id'       : [image.id for image in ProductImage.objects.filter(product_id=product.id)],
-                    'taste'        : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = product.id)],
-                    'taste_id'     : [flavor.taste.id for flavor in TasteByProduct.objects.filter(product_id = product.id)],
+                    'img'            : [{
+                        'img_id'     : image.id,
+                        'img_url'    : image.url
+                    } for image in ProductImage.objects.filter(product_id = product.id)],
+                    'taste'          : [{
+                        'taste_id'   : flavor.taste.name,
+                        'taste_name' : flavor.taste.id
+                    } for flavor in TasteByProduct.objects.filter(product_id = product.id)],
                     'roasting_date': product.roasting_date,
                     'price'        : product.price
                 }for product in products]
