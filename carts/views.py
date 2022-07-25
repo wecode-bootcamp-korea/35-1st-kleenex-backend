b293fc47ef4cfd5a4d07fd5561851a31ff0861d2
apple <apple@apples-MacBook-Pro.local>
2022-07-25 17:10:33 +0900

    Create: carts view(GET)

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from carts.models import Cart
+from products.models import *
+from core.utils import login_decorator
+
+class CartView(View):
+    @login_decorator
+    def get(self, request):
+        cart_list = Cart.objects.filter(user=request.user)
+        result = [{
+            "cart_id": cart.id,
+            "id": cart.product.id,
+            "user": cart.user.name,
+            "product": cart.product.name,
+            "size": cart.size.name,
+            "price": cart.size.price,
+            "graind": cart.graind.type,
+            "quantity": cart.quantity,
+            "image": cart.product.productimage_set.all()[0].url,
+            "is_checked": False
+        } for cart in cart_list]
+
+        return JsonResponse({'MESSAGE': result}, status=200)
