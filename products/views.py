9f03965c5f0262978f3a21db8ba4cdda8d0b26f7
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-25 11:41:26 +0900

    Add img, taste id

--- products/views.py
+++ products/views.py
@@ -16,8 +16,10 @@ class MainProductView(View):
                     'name'          : premium.name,
                     'eng_name'      : premium.eng_name,
                     'img'           : [image.url for image in ProductImage.objects.filter(product_id = premium.id)],
+                    'img_id'        : [image.id for image in ProductImage.objects.filter(product_id = premium.id)],
                     'roasting_date' : premium.roasting_date,
                     'taste'         : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = premium.id)],
+                    'taste_id'      : [flavor.taste.id for flavor in TasteByProduct.objects.filter(product_id = premium.id)],
                     'price'         : premium.price
                 } for premium in premiums]
         
@@ -26,8 +28,10 @@ class MainProductView(View):
                     'name'          : fresh_product.name,
                     'eng_name'      : fresh_product.eng_name,
                     'img'           : [image.url for image in ProductImage.objects.filter(product_id = fresh_product.id)],
+                    'img_id'        : [image.id for image in ProductImage.objects.filter(product_id = fresh_product.id)],
                     'roasting_date' : fresh_product.roasting_date,
                     'taste'         : [flavor.taste.name for flavor in TasteByProduct.objects.filter(product_id = fresh_product.id)],
+                    'taste_id'      : [flavor.taste.id for flavor in TasteByProduct.objects.filter(product_id = fresh_product.id)],
                     'price'         : fresh_product.price
                 } for fresh_product in fresh_products]
 
