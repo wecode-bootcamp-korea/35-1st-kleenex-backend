6efe28d13d3ea92f3d85038274d9506f7da95751
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-26 15:35:31 +0900

    test

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import MainProductView, CoffeeProductView
+from products.views import MainProductView, CoffeeProductView, MainSearchView
 
 urlpatterns = [
     path('/main', MainProductView.as_view()),
     path('', CoffeeProductView.as_view()),
+    path('/main/test', MainSearchView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -5,6 +5,8 @@ from django.views          import View
 
 from products.models  import Product, ProductImage, TasteByProduct
 
+from urllib.parse import unquote
+
 
 class MainProductView(View): 
     def get(self, request): 
@@ -47,43 +49,80 @@ class MainProductView(View):
 
 
 class CoffeeProductView(View):
-    def get(self, request):      
-        page             = int(request.GET.get('page', 1)or 1)
-        category         = request.GET.get('category')or None
-        tastes           = request.GET.getlist('taste')or None
-        filter           = request.GET.getlist('filter')or None
-        page_size        = 12
-        limit            = page_size * page
-        offset           = limit - page_size
-
-        products         = Product.objects.all().order_by('id')
+    def get(self, request):
+        try:
+            page             = int(request.GET.get('page')or 1)
+            category         = request.GET.get('category')
+            tastes           = request.GET.getlist('taste')
+            filter           = request.GET.getlist('filter')
+            page_size        = 12
+            limit            = page_size * page
+            offset           = limit - page_size
     
-        if category:
-            products     = Product.objects.filter(subcategory_id=category).order_by('id')
-        
-        if tastes:
-            products     = products.filter(taste__name__in=tastes).order_by('id').distinct()
-        
-        if filter:
-            if 'Highprice' in filter:
-                products = products.order_by('-price')
-                if 'Highprice' in filter and 'roast' in filter:
-                    products = products.order_by('-roasting_date')
-            elif 'Lowprice' in filter:
-                products = products.order_by('price')
-                if 'Lowprice' in filter and 'roast' in filter:
-                    products = products.order_by('-roasting_date')
-            elif 'roast' in filter:
-                products = products.order_by('-roasting_date')
-        
-        total    = len(products)
-        
-        products = products[offset:limit]
 
-        result_products = [{
-                    'id'           : product.id,
-                    'name'         : product.name,
-                    'eng_name'     : product.eng_name,
+            products         = Product.objects.all().order_by('id')
+        
+            if category:
+                products     = Product.objects.filter(subcategory_id=category).order_by('id')
+            
+            if tastes:
+                products     = products.filter(taste__name__in=tastes).order_by('id').distinct()
+            
+            if filter:
+                if 'Highprice' in filter:
+                    products = products.order_by('-price')
+                    if 'Highprice' in filter and 'roast' in filter:
+                        products = products.order_by('-roasting_date')
+                elif 'Lowprice' in filter:
+                    products = products.order_by('price')
+                    if 'Lowprice' in filter and 'roast' in filter:
+                        products = products.order_by('-roasting_date')
+                elif 'roast' in filter:
+                    products = products.order_by('-roasting_date')
+            
+            total    = len(products)
+            
+            products = products[offset:limit]
+
+            result_products = [{
+                        'id'           : product.id,
+                        'name'         : product.name,
+                        'eng_name'     : product.eng_name,
+                        'img'            : [{
+                            'img_id'     : image.id,
+                            'img_url'    : image.url
+                        } for image in ProductImage.objects.filter(product_id = product.id)],
+                        'taste'          : [{
+                            'taste_id'   : flavor.taste.id,
+                            'taste_name' : flavor.taste.name
+                        } for flavor in TasteByProduct.objects.filter(product_id = product.id)],
+                        'roasting_date': product.roasting_date,
+                        'price'        : product.price
+                    }for product in products]
+
+                
+            return JsonResponse(
+                {'total': total, 'shop_product_list'   : result_products},
+                status = 200
+            )
+        except ValueError:
+            return JsonResponse(
+                {'MESSAGE' : 'Check parameter'},
+                status = 400
+            )
+
+
+
+class MainSearchView(View):
+    def get(self, request):
+        search = request.GET.get('search')
+        key = unquote(search)
+        products = Product.objects.filter(name__icontains=key)
+
+        result = [{  
+                    'id'             : product.id,
+                    'name'           : product.name,
+                    'eng_name'       : product.eng_name,
                     'img'            : [{
                         'img_id'     : image.id,
                         'img_url'    : image.url
@@ -92,12 +131,9 @@ class CoffeeProductView(View):
                         'taste_id'   : flavor.taste.id,
                         'taste_name' : flavor.taste.name
                     } for flavor in TasteByProduct.objects.filter(product_id = product.id)],
-                    'roasting_date': product.roasting_date,
-                    'price'        : product.price
+                    'roasting_date'  : product.roasting_date,
+                    'price'          : product.price
                 }for product in products]
 
-            
-        return JsonResponse(
-            {'total': total, 'shop_product_list'   : result_products},
-            status = 200
-        )
+
+        return JsonResponse({'MESSAGE' : result}, status =200)
