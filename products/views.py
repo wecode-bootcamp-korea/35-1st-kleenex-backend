1f537f2cf8e0ec53715900e6b43062c1d67edcb3
jongbeom <jongbeom@ip-10-58-7-60.ap-northeast-2.compute.internal>
2022-07-27 10:52:13 +0900

    code_review_product

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http           import JsonResponse
 from django.views          import View
+from django.db.models      import Q
 
 from products.models  import Product, ProductImage, TasteByProduct, Grainding, Size
 
@@ -48,37 +49,56 @@ class MainProductView(View):
 
 class CoffeeProductView(View):
     def get(self, request):      
-        page             = int(request.GET.get('page', 1)or 1)
-        category         = request.GET.get('category')or None
-        tastes           = request.GET.getlist('taste')or None
-        filter           = request.GET.getlist('filter')or None
-        page_size        = 12
-        limit            = page_size * page
-        offset           = limit - page_size
+        """
+        python dictionary
+        dict = {
+            a : 1,
+            b : 2, 
+            c : 3
+        }
 
-        products         = Product.objects.all().order_by('id')
+        dict['d'] // raise KeyError
+        dict.get('d') // 
+        """
+        category         = request.GET.get('category')
+        tastes           = request.GET.getlist('taste')
+        sorting          = request.GET.get('sorting', 'id')
+        offset           = int(request.GET.get('offset', 0))
+        limit            = int(request.GET.get('limin', 9))
+
+        # products         = Product.objects.all().order_by('id')
     
+        # if category:
+        #     products     = Product.objects.filter(subcategory_id=category).order_by('id')
+        
+        # if tastes:
+        #     products     = products.filter(taste__name__in=tastes).order_by('id').distinct()
+
+        q = Q()
+
         if category:
-            products     = Product.objects.filter(subcategory_id=category).order_by('id')
-        
+            q &= Q(subcategory_id=category)
+
         if tastes:
-            products     = products.filter(taste__name__in=tastes).order_by('id').distinct()
+            q &= Q(taste__name__in=tastes)
+
+        sort_dict = {
+            'Highprice' : '-price',
+            'Lowprice'  : 'price',
+            'roast'     : '-roasting_date',
+            'id'        : 'id'
+        }
+
+        products = Product.objects.filter(q).order_by(sort_dict.get(sorting))[offset:offset+limit]
+
         
-        if filter:
-            if 'Highprice' in filter:
-                products = products.order_by('-price')
-                if 'Highprice' in filter and 'roast' in filter:
-                    products = products.order_by('-roasting_date')
-            elif 'Lowprice' in filter:
-                products = products.order_by('price')
-                if 'Lowprice' in filter and 'roast' in filter:
-                    products = products.order_by('-roasting_date')
-            elif 'roast' in filter:
-                products = products.order_by('-roasting_date')
-        
-        total    = len(products)
-        
-        products = products[offset:limit]
+        # if filter:
+        #     if 'Highprice' in filter:
+        #         products = products.order_by('-price')
+        #     elif 'Lowprice' in filter:
+        #         products = products.order_by('price')
+        #     elif 'roast' in filter:
+        #         products = products.order_by('-roasting_date')
 
         result_products = [{
                     'id'             : product.id,
@@ -97,7 +117,10 @@ class CoffeeProductView(View):
                 }for product in products]
 
         return JsonResponse(
-            {'total' : total, 'shop_product_list'   : result_products},
+            {
+                'total'             : len(products), 
+                'shop_product_list' : result_products
+            },
             status = 200
         )
 
@@ -138,3 +161,29 @@ class ProductDetailView(View):
         except Product.DoesNotExist:
             return JsonResponse({'MESSAGE' : 'Product matching query does not exist.'}, status = 404)
             
+
+class MainSearchView(View):
+    def get(self, request):
+        search   = request.GET.get('keywords')
+        products = Product.objects.filter(name__icontains=unquote(search))
+
+        result = [{  
+                    'id'             : product.id,
+                    'name'           : product.name,
+                    'eng_name'       : product.eng_name,
+                    'img'            : [{
+                        'img_id'     : image.id,
+                        'img_url'    : image.url
+                    } for image in ProductImage.objects.filter(product_id = product.id)],
+                    'taste'          : [{
+                        'taste_id'   : flavor.taste.id,
+                        'taste_name' : flavor.taste.name
+                    } for flavor in TasteByProduct.objects.filter(product_id = product.id)],
+                    'roasting_date'  : product.roasting_date,
+                    'price'          : product.price
+                }for product in products]
+
+        if len(products) == 0:
+            return JsonResponse({'MESSAGE' : 'NO RESULT'}, status=404)
+
+        return JsonResponse({'result' : result}, status =200)
