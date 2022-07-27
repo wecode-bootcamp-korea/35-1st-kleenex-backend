27a98e1b2eab5674f829e01b97a86366ff281e17
apple <apple@apples-MacBook-Pro.local>
2022-07-27 17:32:15 +0900

    Create: carts view(delete) (9)

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,6 @@
 import json
 
-<<<<<<< HEAD
-=======
 from django.db import transaction
->>>>>>> main
 from django.http import JsonResponse
 from django.views import View
 
@@ -12,27 +9,7 @@ from products.models import *
 from core.utils import login_decorator
 
 class CartView(View):
-<<<<<<< HEAD
     @login_decorator
-    def delete(self, request):
-        try:
-            datas = json.loads(request.body)
-            user = request.user 
-            if datas.get("is_bool"):
-                Cart.objects.filter(user=user).delete()
-
-            elif datas.get("cart_id"):
-                for data in datas["cart_id"]:
-                    Cart.objects.get(id=data, user=user).delete()
-
-            return JsonResponse({"MESSAGE": "DELETE_SUCCESS"}, status=200)
-
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
-
-        except Cart.DoesNotExist:
-            return JsonResponse({"MESSAGE": "DOESNOTEXIST_CART"}, status=400)
-=======
     def get(self, request):
         cart_list = Cart.objects.select_related('product','user','size','graind')\
                                 .prefetch_related('product__productimage_set')\
@@ -121,4 +98,23 @@ class CartView(View):
 
         except Cart.DoesNotExist:
             return JsonResponse({"MESSAGE": "DOESNOTEXIST_CART"}, status=400)
->>>>>>> main
+
+    @login_decorator
+    def delete(self, request):
+        try:
+            datas = json.loads(request.body)
+            user = request.user 
+            if datas.get("is_bool"):
+                Cart.objects.filter(user=user).delete()
+
+            elif datas.get("cart_id"):
+                for data in datas["cart_id"]:
+                    Cart.objects.get(id=data, user=user).delete()
+
+            return JsonResponse({"MESSAGE": "DELETE_SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+        except Cart.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOESNOTEXIST_CART"}, status=400)
