2146b4343fdf9c4832cf86e4fdfbc1603eab043a
apple <apple@apples-MacBook-Pro.local>
2022-07-27 15:15:46 +0900

    Create: carts view(post) (10)

--- carts/views.py
+++ carts/views.py
@@ -10,52 +10,6 @@ from core.utils import login_decorator
 
 class CartView(View):
     @login_decorator
-    def post(self, request):
-        try:
-            datas           = json.loads(request.body)
-            user            = request.user
-            product_id      = datas["product_id"]
-            products        = datas["product"]
-            target_product  = Product.objects.get(id = product_id)
-
-            with transaction.atomic():
-                for product in products:
-                    quantity    = product["quantity"]
-                    graind      = target_product.graindbyproduct_set.get(grainding_id = product["graind"]).grainding
-                    size        = target_product.size_set.get(name = product["size"])
-
-                    cart, is_bool   = Cart.objects.get_or_create(
-                        user        = user,
-                        product     = target_product,
-                        graind      = graind,
-                        size        = size,
-                        defaults    = {'quantity': quantity}
-                    )
-
-                    if is_bool == False:
-                        cart.quantity = cart.quantity + quantity
-                        cart.save()
-
-                return JsonResponse({"MESSAGE": "TEST"}, status=200)
-
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEYERROR"}, status=400)
-
-        except Size.DoesNotExist:
-            return JsonResponse({"MESSAGE": "DOESNOTEXIST_SIZE"}, status=400)
-
-        except Product.DoesNotExist:
-            return JsonResponse({"MESSAGE": "DOESNOTEXIST_PRODUCT"}, status=400)
-
-        except Grainding.DoesNotExist:
-            return JsonResponse({"MESSAGE": "DOESNOTEXIST_GRAINDING"}, status=400)
-
-        except json.JSONDecodeError:
-            return JsonResponse
-
-        except GraindByProduct.DoesNotExist:
-            return JsonResponse({"MESSAGE": "DOESNOTEXIST_GRAINDING"}, status=400)
-
     def get(self, request):
         cart_list = Cart.objects.select_related('product','user','size','graind')\
                                 .prefetch_related('product__productimage_set')\
@@ -75,3 +29,50 @@ class CartView(View):
         } for cart in cart_list]
 
         return JsonResponse({'MESSAGE': result}, status=200)
+
+    @login_decorator
+    @transaction.atomic()
+    def post(self, request):
+        try:
+            datas           = json.loads(request.body)
+            user            = request.user
+            product_id      = datas["product_id"]
+            products        = datas["product"]
+            target_product  = Product.objects.get(id = product_id)
+            
+            for product in products:
+                quantity    = product["quantity"]
+                graind      = target_product.graindbyproduct_set.get(grainding_id = product["graind"]).grainding
+                size        = target_product.size_set.get(name = product["size"])
+
+                cart, is_bool   = Cart.objects.get_or_create(
+                    user        = user,
+                    product     = target_product,
+                    graind      = graind,
+                    size        = size,
+                    defaults    = {'quantity': quantity}
+                )
+
+                if is_bool == False:
+                    cart.quantity += quantity
+                    cart.save()
+
+            return JsonResponse({"MESSAGE": "TEST"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEYERROR"}, status=400)
+
+        except Size.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOESNOTEXIST_SIZE"}, status=400)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOESNOTEXIST_PRODUCT"}, status=400)
+
+        except Grainding.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOESNOTEXIST_GRAINDING"}, status=400)
+
+        except json.JSONDecodeError:
+            return JsonResponse
+
+        except GraindByProduct.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOESNOTEXIST_GRAINDING"}, status=400)
